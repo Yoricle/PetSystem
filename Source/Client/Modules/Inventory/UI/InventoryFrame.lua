75195388f1711697ab2f2864db471a3a869cbf82
Kyle Hackett <kylehackett1@gmail.com>
2023-04-30 14:43:19 +0100

    Roact Tree Updates

--- Source/Client/Modules/Inventory/UI/InventoryFrame.lua
+++ Source/Client/Modules/Inventory/UI/InventoryFrame.lua
@@ -7,6 +7,7 @@ local InventoryFrame = Roact.Component:extend("InventoryFrame")
 function InventoryFrame:init()
     self:setState({
         value = self.props.viewModel.value,
+        popUpText = self.props.viewModel.popUpText,
     })
 end
 
@@ -25,10 +26,10 @@ function InventoryFrame:render()
         BackgroundTransparency = 1,
         Position = UDim2.new(0.5, 0, 1, 0),
         Size = UDim2.new(0.75, 0, 0.1, 50),
-
     },{
         Inventory = Roact.createElement("TextButton", {
             Name = "Inventory",
+            Text = "Inventory",
             LayoutOrder = 1,
             Size = UDim2.new(0.6, 0, 0.6,0),
             Position = UDim2.new(0.5, 0, 0.5, 0),
@@ -37,12 +38,62 @@ function InventoryFrame:render()
                 AspectRatio = 1,
             }),
         }),
-    },{
-        Shop = Roact.createElement("TextButton",{
+
+
+        Shop = Roact.createElement("TextButton", {
             Name = "Shop",
+            Text = "Shop",
             LayoutOrder = 5,
             Size = UDim2.new(0.6, 0, 0.6,0),
             Position = UDim2.new(0.5, 0, 0.5, 0),
+        },{
+            UIAspectRatioConstraint = Roact.createElement("UIAspectRatioConstraint", {
+                AspectRatio = 1,
+            }), 
+        }),
+
+        Settings = Roact.createElement("TextButton", {
+            Name = "Settings",
+            Text = "Settings",
+            LayoutOrder = 6,
+            Size = UDim2.new(0.6, 0, 0.6,0),
+            Position = UDim2.new(0.5, 0, 0.5, 0),
+        },{
+            UIAspectRatioConstraint = Roact.createElement("UIAspectRatioConstraint", {
+                AspectRatio = 1,
+            }),  
+        }),
+
+        Trading = Roact.createElement("TextButton", {
+            Name = "Trading",
+            Text = "Trading",
+            LayoutOrder = 3,
+            Size = UDim2.new(0.6, 0, 0.6,0),
+            Position = UDim2.new(0.5, 0, 0.5, 0),
+        },{
+            UIAspectRatioConstraint = Roact.createElement("UIAspectRatioConstraint", {
+                AspectRatio = 1,
+            }),   
+        }),
+
+        Achievements = Roact.createElement("TextButton", {
+            Name = "Achievements",
+            Text = "Achievements",
+            LayoutOrder = 4,
+            Size = UDim2.new(0.6, 0, 0.6,0),
+            Position = UDim2.new(0.5, 0, 0.5, 0),
+        },{
+            UIAspectRatioConstraint = Roact.createElement("UIAspectRatioConstraint", {
+                AspectRatio = 1,
+            }),
+        }),
+
+        UIListLayout = Roact.createElement("UIListLayout", {
+            Padding = UDim.new(0.01, 10),
+            FillDirection = Enum.FillDirection.Horizontal,
+            HorizontalAlignment = Enum.HorizontalAlignment.Center,
+            SortOrder = Enum.SortOrder.LayoutOrder,
+            VerticalAlignment = Enum.VerticalAlignment.Center,
         })
     })
 end
--- Source/Client/Modules/Inventory/UI/InventoryViewModel.lua
+++ Source/Client/Modules/Inventory/UI/InventoryViewModel.lua
@@ -11,12 +11,18 @@ function InventoryViewModel.new(): table
     local self = ViewModel.new()
 
     self.value = 0
+    self.popUpText = "" -- this will changed based on which button is hovered over
 
     return setmetatable(self, InventoryViewModel)
 end
 
 function InventoryViewModel:setValue(newValue: any)
     self.value = newValue
+    self:update()
+end
+
+function InventoryViewModel:setPopupTitle(newValue: any)
+    self.popUpText = newValue
     self:update()
 end
 
