811568aa00e60c657efbbc9e46139430321176d8
Kyle Hackett <kylehackett1@gmail.com>
2023-05-06 23:27:46 +0100

    Inventory Changes

---
+++ Source/Client/Modules/Inventory/UI/MainInventory.lua
@@ -0,0 +1,14 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local Roact = require(ReplicatedStorage.Packages.Roact)
+
+local Flipper = require(ReplicatedStorage.Packages.Flipper)
+local RoactFlipper = require(ReplicatedStorage.Packages.RoactFlipper)
+
+local MainInventory = Roact.Component:extend("MainInventory")
+
+function MainInventory:init()
+    self:setState({
+        
+    })
+end
