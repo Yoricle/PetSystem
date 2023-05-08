617aeef3d4ac22d96b99eab733d1335449d18e27
Kyle Hackett <kylehackett1@gmail.com>
2023-05-08 04:14:35 +0100

    Pet Data

--- Source/Shared/Modules/Data/PlayerDataTemplate.lua
+++ Source/Shared/Modules/Data/PlayerDataTemplate.lua
@@ -4,10 +4,12 @@
 ]]
 
 return {
-    ["Gems"] = 0,
-    ["Coins"] = 0,
-    ["RAP"] = 0,
-    ["Stars"] = 0
+    ["Gems"] = 0, -- valuable game currency (harder to obtain)
+    ["Coins"] = 0, -- standard game currency
+    ["RAP"] = 0, -- meaning pet value and value system
+    ["Stars"] = 0, -- earned when incrementing villages, basically experience
+    ["Village"] = 0, -- what village you've reached
+    ["Shields"] = 0, -- basically shields you from other players attacking
 }
 
 
